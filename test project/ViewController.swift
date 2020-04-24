c8e9b4d18e6b9dbc26f649f2fe222e3295f69db6
Vadim Khmyrov <43887848+vkhmyrov@users.noreply.github.com>
2020-04-24 15:11:03 +0300

    Initial Commit

---
+++ test project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test project
+//
+//  Created by Vadim Khmyrov on 24.04.2020.
+//  Copyright © 2020 Vadim Khmyrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
